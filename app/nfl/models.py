1abdfe76e83aaf5508482afd69338804ef3b15fc
Marcel Konrad <makonrad@gmx.de>
2020-04-06 16:48:35 +0200

    Added custom model manager to evaluate whole weeks and doc strings

--- app/nfl/models.py
+++ app/nfl/models.py
@@ -1,5 +1,5 @@
 from datetime import date
-from typing import Tuple
+from typing import Dict, List, Tuple
 
 from django.conf import settings
 from django.db import models
@@ -7,6 +7,7 @@ from django.db.models import F, Q
 from django.db.models.aggregates import Count
 from pytz import timezone
 
+from core.models import PickPoolUser
 from nfl.defines import CityChoices, PickChoices, StadiumChoices, TeamChoices
 
 est_tz = timezone("EST")
@@ -79,12 +80,55 @@ class Year(models.Model):
         return f"Year: {self.year}"
 
 
+class WeekManager(models.Manager):
+    def evaluate_week(
+        self, year: int, week: int
+    ) -> List[Tuple[int, List[PickPoolUser]]]:
+        """Evaluate a whole week.
+
+        Parameters
+        ----------
+        models : WeekManager
+            Django model manager
+        year : int
+            Year to evaluate
+        week : int
+            Week to evaluate
+
+        Returns
+        -------
+        List[Tuple[int, List[PickPoolUser]]]
+            List of tuples containing earned points with corresponding users.
+        """
+        try:
+            cur_week = self.get(year__year=year, week=week)
+            user_points = {}
+            for game in cur_week.games.all():
+                game_points = game.evaluate_game()
+                for pick, points in game_points.items():
+                    if pick in user_points:
+                        user_points[pick.user] += points
+                    else:
+                        user_points[pick.user] = points
+
+            res = {}
+            for user, points in user_points.items():
+                if points in res:
+                    res[points].append(user)
+                else:
+                    res[points] = [user]
+            return sorted(res, reverse=True)
+        except Week.DoesNotExist:
+            return {}
+
+
 class Week(models.Model):
     class Meta:
         unique_together = ["year", "week"]
 
     week = models.PositiveSmallIntegerField()
     year = models.ForeignKey(Year, on_delete=models.CASCADE)
+    objects = WeekManager()
 
     def __str__(self) -> str:
         return f"Week: {self.week}, {self.year}"
@@ -105,6 +149,13 @@ class Game(models.Model):
 
     @property
     def winner(self) -> PickChoices:
+        """Determine the winner team of this game.
+
+        Returns
+        -------
+        PickChoices
+            If this game is final returns the winning team or tie.
+        """
         if not self.final:
             return None
         if self.home_team_score == self.visitor_team_score:
@@ -113,7 +164,26 @@ class Game(models.Model):
             return PickChoices.HOME_TEAM
         return PickChoices.VISITOR_TEAM
 
+    def evaluate_game(self) -> Dict["Pick", int]:
+        """Evaluate all picks of a game.
+
+        Returns:
+            Dict[int, List[Pick]]
+                Mapping of picks with their corresponding points earned.
+        """
+        user_points = {}
+        for pick in self.picks.all():
+            user_points[pick] = pick.awarded_points
+        return user_points
+
     def is_monday_night(self) -> bool:
+        """Check if this game is a monday night game.
+
+        Returns
+        -------
+        bool
+            True if this game is a monday night game, False otherwise.
+        """
         ts_est = self.timestamp.astimezone(est_tz)
         if ts_est.weekday() == 0:
             return True
@@ -134,10 +204,39 @@ class Pick(models.Model):
     picked_tie_break = models.PositiveSmallIntegerField(default=0)
 
     @property
-    def awarded_points(self):
+    def awarded_points(self) -> int:
+        """This pick's points earned
+
+        Returns
+        -------
+        int
+            Points earned, if any
+        """
         if self.selection == self.game.winner:
             return 1
         return 0
+
+    @property
+    def tie_break(self) -> int:
+        """This pick's tie break after the game has finished.
+
+        Returns
+        -------
+        int
+            This pick's tie break.
+        """
+        if not self.game.final:
+            return None
+        tb = abs(self.game.home_team_score - self.game.visitor_team_score)
+        game_winner = self.game.winner
+        if self.selection == game_winner:
+            return abs(tb - self.picked_tie_break)
+        else:
+            if game_winner == PickChoices.HOME_TEAM:
+                return self.game.home_team_score + self.picked_tie_break
+            elif game_winner == PickChoices.VISITOR_TEAM:
+                return self.game.visitor_team_score + self.picked_tie_break
+        return self.picked_tie_break
 
     def __str__(self) -> str:
         return f"{self.user.first_name} picked '{PickChoices(self.selection).label}' for {self.game}"
