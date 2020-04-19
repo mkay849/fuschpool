29fcd2e448426a4b313cfd24b8c1ba75f62ae03b
Marcel Konrad <makonrad@gmx.de>
2020-04-19 15:32:36 +0200

    Extended Teams view to support any season / week combination
    And black auto formatting source file

--- app/nfl/urls.py
+++ app/nfl/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from nfl.views import ScheduleView, StandingsView, TeamsView, PicksView
 
-app_name = 'nfl'
+app_name = "nfl"
 urlpatterns = [
-    path('', StandingsView.as_view(), name='standings'),
-    path('teams/', TeamsView.as_view(), name='teams'),
-    path('schedule/', ScheduleView.as_view(), name='schedule'),
-    path('picks/', PicksView.as_view(), name='make_picks'),
+    path("", StandingsView.as_view(), name="standings"),
+    path("teams/", TeamsView.as_view(), name="teams",),
+    path("teams/<int:season>/", TeamsView.as_view(), name="teams",),
+    path("teams/<int:season>/<int:week>/", TeamsView.as_view(), name="teams",),
+    path("schedule/", ScheduleView.as_view(), name="schedule"),
+    path("picks/", PicksView.as_view(), name="make_picks"),
 ]
